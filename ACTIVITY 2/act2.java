9e3c1ad57194db43fbe479d8b4e41fab7dfc9bdf
OBJPROG8025 <OBJPROG8025@lab.cict>
2023-02-01 21:03:09 +0900

    Activity 2 Completed

---
+++ ACTIVITY 2/act2.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+
+public class act2 {
+    public static void main(String[] args) {
+        int number = -10;
+        if (number == 0) {
+        System.out.println("Enter the number you showed: " + number);
+        System.out.println("Zero");
+        } else if (number > 0) {
+         int square = number * number;
+         System.out.println("Enter the number you showed: " + number);
+         System.out.println("Square = " + square);
+        } else {
+        int cube = number * number * number;
+        System.out.println("Enter the number you showed: " + number);
+        System.out.println("Cube = " + cube);
+        }
+        }
+        }
+        
+        
