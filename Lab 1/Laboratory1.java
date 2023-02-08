f4699260ee8eb84da201eaeab4907ad3c0897613
OBJPROG8025 <OBJPROG8025@lab.cict>
2023-02-08 18:32:52 +0800

    Laboratory 1 completed as your final commit

---
+++ Lab 1/Laboratory1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Laboratory1 {
+    static int Lavoratory1; 
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your grade: ");
+        int grade = scanner.nextInt();
+        
+        if (grade >= 90) {
+            System.out.println("You got an A!");
+        } else if (grade >= 80 && grade <= 89) {
+            System.out.println("You got an B!");
+        } else if (grade >= 70 && grade <= 79) {
+            System.out.println("You got an C!");
+        } else if (grade >= 60 && grade <= 69) {
+            System.out.println("You got an D!");
+        } else {
+            System.out.println("You got an F!");
+        }
+    }
+}
