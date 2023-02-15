1b8b3e56ad5fcd223950c0dbdacaba1ccd3469ef
Hannah Farol <haneynahbrdcn@gmail.com>
2023-02-15 19:28:10 +0800

    Digit Sum machine problem completed

---
+++ DigitSum/Digitsum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Digitsum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        System.out.println("The sum of the digits is: " + sum);
+        
+    }
+}
+
